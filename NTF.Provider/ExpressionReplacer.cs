9f01047c380b1da3c5a62179784d406c297f0c0b
Tzen <doutzen56@163.com>
2017-06-07 17:21:28 +0800

    Provider集成 Signed-off-by: Tzen <doutzen56@163.com>

---
+++ NTF.Provider/ExpressionReplacer.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+
+namespace NTF.Provider
+{
+    /// <summary>
+    /// Replaces references to one specific instance of an expression node with another node
+    /// </summary>
+    public class ExpressionReplacer : ExpressionVisitor
+    {
+        Expression searchFor;
+        Expression replaceWith;
+
+        private ExpressionReplacer(Expression searchFor, Expression replaceWith)
+        {
+            this.searchFor = searchFor;
+            this.replaceWith = replaceWith;
+        }
+
+        public static Expression Replace(Expression expression, Expression searchFor, Expression replaceWith)
+        {
+            return new ExpressionReplacer(searchFor, replaceWith).Visit(expression);
+        }
+
+        public static Expression ReplaceAll(Expression expression, Expression[] searchFor, Expression[] replaceWith)
+        {
+            for (int i = 0, n = searchFor.Length; i < n; i++)
+            {
+                expression = Replace(expression, searchFor[i], replaceWith[i]);
+            }
+            return expression;
+        }
+
+        protected override Expression Visit(Expression exp)
+        {
+            if (exp == this.searchFor)
+            {
+                return this.replaceWith;
+            }
+            return base.Visit(exp);
+        }
+    }
+}
