99ddeb7b2e9eb499e070524ab4a07051cb367c69
tzen <tzen@mail.eveb>
2017-09-08 15:05:29 +0800

    日志统一接口添加，并提供默认实现 Signed-off-by: tzen <tzen@mail.eveb>

---
+++ NTF/Logger/INtfLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NTF.Logger
+{
+    /// <summary>
+    /// 日志基类
+    /// </summary>
+    public interface INtfLog
+    {
+        void Info(string msg);
+        void Info(Exception ex);
+        void Error(string msg);
+        void Error(Exception ex);
+        void Fatal(string msg);
+        void Fatal(Exception ex);
+        void Trace(string msg);
+        void Trace(Exception ex);
+        void Warn(Exception ex);
+        void Warn(string msg);
+    }
+}
---
+++ NTF/Logger/NullLoger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace NTF.Logger
+{
+    /// <summary>
+    /// 默认日志实现
+    /// </summary>
+    /// <remarks>
+    /// 底层默认提供一个空的日志实现，调用层通过注入对应日志实现类来写日志
+    /// </remarks>
+    internal class NullLoger : INtfLog
+    {
+        public void Error(Exception ex)
+        {
+            
+        }
+
+        public void Error(string msg)
+        {
+            
+        }
+
+        public void Fatal(Exception ex)
+        {
+            
+        }
+
+        public void Fatal(string msg)
+        {
+            
+        }
+
+        public void Info(Exception ex)
+        {
+            
+        }
+
+        public void Info(string msg)
+        {
+            
+        }
+
+        public void Trace(Exception ex)
+        {
+            
+        }
+
+        public void Trace(string msg)
+        {
+            
+        }
+
+        public void Warn(string msg)
+        {
+            
+        }
+
+        public void Warn(Exception ex)
+        {
+            
+        }
+    }
+}
