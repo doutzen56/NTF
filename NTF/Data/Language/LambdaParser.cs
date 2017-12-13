3c3b830bb854d98d7fac7853637edee07eaaa489
tzen <doutzen56@163.com>
2017-12-13 08:33:11 +0800

    表达式解析
    Signed-off-by: tzen <doutzen56@163.com>

---
+++ NTF/Data/Language/LambdaParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTF.Data.Language
+{
+    /// <summary>
+    /// <see cref="Expression"/>谓语解析器，主要负责将<see cref="Expression"/>翻译成T-SQL的Where条件语句
+    /// </summary>
+    public abstract class LambdaParser : ExpressionVisitor
+    {
+        /// <summary>
+        /// T-SQL Where条件
+        /// </summary>
+        public abstract StringBuilder PredicateText { get; set; }
+        /// <summary>
+        /// 由谓语解析器<see cref="LambdaParser"/>翻译的<see cref="PredicateText"/>语句中对应参数的值
+        /// </summary>
+        public abstract dynamic Parameters { get; set; }
+        /// <summary>
+        /// 用于动态生成<see cref="PredicateText"/>语句中变量与参数用
+        /// </summary>
+        private int Index = 0;
+    }
+}
---
+++ NTF/Data/Language/SQLFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTF.Data.Language
+{
+    /// <summary>
+    /// 构建T-SQL语句
+    /// </summary>
+    public abstract class SQLFormatter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract StringBuilder CommandText { get; set; }
+    }
+}
