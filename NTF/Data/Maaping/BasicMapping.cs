5dd7f880334adfaf07fb1eeb2f7cbdd646fbded0
tzen <doutzen56@163.com>
2017-12-15 03:14:29 +0800

    缓存基类添加 Signed-off-by: tzen <doutzen56@163.com>

---
+++ NTF/Data/Maaping/BasicMapping.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTF.Data.Maaping
+{
+    /// <summary>
+    /// 实体映射基类
+    /// </summary>
+    public abstract class BasicMapping
+    {
+        /// <summary>
+        /// 实体对应的表名
+        /// </summary>
+        public virtual string TableName { get; }
+        /// <summary>
+        /// 实体对应的表别名
+        /// </summary>
+        public virtual string TableAlias { get; }
+        /// <summary>
+        /// 实体属性
+        /// </summary>
+        public static PropertyInfo[] Properties;
+        public static PropertyInfo[] KeyProperties;
+        public static PropertyInfo AutoIncrement;
+
+        /// <summary>
+        /// 获取实体对应的表名
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        protected virtual string GetTableName(Type type)
+        {
+            return type.Name;
+        }
+    }
+}
